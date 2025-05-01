842bd68a9165fb19c39c8ad6bce8114a0afed36e
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-01 14:59:03 +0300

    chore: declare ft_memcpy prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:17:36 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/05/01 14:50:40 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/05/01 14:57:08 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
