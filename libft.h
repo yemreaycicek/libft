3b80cfddeefe56e6c38e057b1003a1d5e856ee67
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-03 12:04:08 +0300

    chore: declare ft_toupper prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:17:36 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/05/02 10:56:31 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/05/03 12:02:59 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int	ft_toupper(int c);
 
 #endif
