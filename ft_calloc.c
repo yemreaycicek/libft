c18b1e4c39b32220e306ec9a6df4dfb81403c161
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-06 12:55:12 +0300

    feat: implement ft_calloc function

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 12:44:23 by yaycicek          #+#    #+#             */
+/*   Updated: 2025/05/06 12:52:20 by yaycicek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total;
+	void	*ptr;
+	
+	total = nmemb * size;
+	ptr = (void *)malloc(total);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, total);
+	return (ptr);
+}
