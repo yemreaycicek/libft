f9c1d83712119298b8e7b7a4321191489fbc87c2
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-06 12:40:48 +0300

    chore: declare ft_atoi prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:17:36 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/05/05 13:43:25 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/05/06 12:39:34 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int	ft_atoi(const char *nptr);
 
 #endif
