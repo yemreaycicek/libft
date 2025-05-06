016d0dcccdf5693c91da91f47e646c9fdbf8bd41
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-06 12:55:25 +0300

    chore: declare ft_calloc prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,14 +6,14 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:17:36 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/05/06 12:39:34 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/05/06 12:53:24 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
+# include <stdlib.h>
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
@@ -36,5 +36,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int	ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
