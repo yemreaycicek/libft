081350bf0b82ca600ee0245406774e80fc46f25d
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-08 14:08:38 +0300

    feat: implement ft_substr function

---
+++ ft_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:04:12 by yaycicek          #+#    #+#             */
+/*   Updated: 2025/05/08 14:07:47 by yaycicek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	s_len;
+	size_t	sub_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	sub_len = s_len - start;
+	if (len < sub_len)
+		sub_len = len;
+	substr = (char *)malloc(sizeof(char) * (sub_len + 1));
+	if (!substr)
+		return (NULL);
+	ft_memcpy(substr, s + start, sub_len);
+	substr[sub_len] = '\0';
+	return (substr);
+}
+	
