925e721af611f059195258926074309b52e4d3c9
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-08 22:57:47 +0300

    feat: implement ft_split function

---
+++ ft_split.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 21:57:54 by yaycicek          #+#    #+#             */
+/*   Updated: 2025/05/08 22:46:07 by yaycicek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static size_t	ft_word_count(char const *s, char c)
+{
+	size_t	count;
+	int		in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && !in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+
+static size_t	ft_word_len(char const *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s && *s != c)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+static void	free_array(char **array, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+static char	*ft_word_dup(char const *s, size_t len)
+{
+	char	*word;
+	size_t	i;
+
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**array;
+	size_t	word_count;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	word_count = ft_word_count(s, c);
+	array = (char **)malloc(sizeof(char *) * (word_count + 1));
+	if (!array)
+		return (NULL);
+	i = -1;
+	while (++i < word_count)
+	{
+		while (*s == c)
+			s++;
+		array[i] = ft_word_dup(s, ft_word_len(s, c));
+		if (!array[i])
+		{
+			free_array(array, i);
+			return (NULL);
+		}
+		s += ft_word_len(s, c);
+	}
+	array[i] = NULL;
+	return (array);
+}
