f93fec1a75e57b34507fa06fb9f63dcbb3bf4fa6
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-08 14:20:53 +0300

    feat: implement ft_strtrim function

---
+++ ft_strtrim.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:14:20 by yaycicek          #+#    #+#             */
+/*   Updated: 2025/05/08 14:18:34 by yaycicek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_is_in_set(char c, const char *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+	size_t	start;
+	size_t	end;
+	size_t	len;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_is_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_is_in_set(s1[end - 1], set))
+		end--;
+	len = end - start;
+	trimmed = (char *)malloc(sizeof(char) * (len + 1));
+	if (!trimmed)
+		return (NULL);
+	ft_memcpy(trimmed, s1 + start, len);
+	trimmed[len] = '\0';
+	return (trimmed);
+}
