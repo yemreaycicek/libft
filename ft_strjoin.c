543efa2bcf6afa7cd83820bc918e8029acf53b86
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-08 14:15:39 +0300

    feat: implement ft_strjoin function

---
+++ ft_strjoin.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:11:20 by yaycicek          #+#    #+#             */
+/*   Updated: 2025/05/08 14:11:26 by yaycicek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	total_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	total_len = s1_len + s2_len;
+	join = (char *)malloc(sizeof(char) * (total_len + 1));
+	if (!join)
+		return (NULL);
+	ft_memcpy(join, s1, s1_len);
+	ft_memcpy(join + s1_len, s2, s2_len);
+	join[total_len] = '\0';
+	return (join);
+}
