551a414c3e24ffcd0902c9ebefe745ad9653d4b9
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-10 15:45:46 +0300

    fix: add missing include in ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,9 +6,11 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 15:35:27 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/05/09 15:35:33 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/05/10 15:32:49 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	ft_putendl_fd(char *s, int fd)
 {
