ddce6ab3dc5ca557f1ac4bac4608b5bcf1b8d6b9
yaycicek <yunus.e.aycicek@gmail.com>
2025-05-11 00:40:06 +0300

    chore: declare ft_lstdelone prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:17:36 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/05/11 00:33:57 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/05/11 00:38:15 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int	ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 
 #endif
